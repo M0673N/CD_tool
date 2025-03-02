b557dfe9465100b7e0982972c2026a90ad884c9e
M0673N <stoyan_valkov@protonmail.com>
2025-03-03 01:10:44 +0200

    moved database string in env

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///users.db")
+load_dotenv()
+
+DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
