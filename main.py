a5edf44dfb617b4f5f11994623455dc3579d00f1
M0673N <stoyan_valkov@protonmail.com>
2025-03-02 22:29:09 +0200

    improved design

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routes import router
 
 
 app = FastAPI()
 app.include_router(router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     import uvicorn
